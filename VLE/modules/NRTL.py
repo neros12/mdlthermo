e8a489250b410bb172bd5022b5135dd013c8757c
NewSeo <neros12@naver.com>
2024-12-04 15:18:57 +0900

    depenency update

--- VLE/modules/NRTL.py
+++ VLE/modules/NRTL.py
@@ -1,17 +1,11 @@
-import os
-import sys
+"""
+Deprecated Code
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import vapor_pressure.equations as vp
-
-
-A12 = 2.3015
-A21 = -0.5363
-B12 = -879.7008
-B21 = 1412.7316
-C = 0.3
 
 
 def NRTL(x1, x2, T, a12, a21, b12, b21, c):
@@ -94,47 +88,3 @@ def Bisection(
             xl = xr
 
     return xr
-
-
-# component_2 = {"CASRN": "7732185"}
-# component_1 = {"CASRN": "108952"}
-# vapor_pressure_model = vp.Cal_Vapor_Pressure
-# Patm = 101.325
-# X1 = np.arange(0.01, 1, 0.01)
-# Xmin = 315
-# Xmax = 640
-
-# Y1 = []
-# Ts = []
-# for x1 in [0.88]:
-#     x2 = 1 - x1
-
-#     Tsol = Bisection(
-#         Xmin,
-#         Xmax,
-#         Modifed_Raults_Law,
-#         c1=component_1,
-#         c2=component_2,
-#         x1=x1,
-#         x2=x2,
-#         activity_model=NRTL,
-#         vapor_pressure_model=vapor_pressure_model,
-#     )
-
-#     if Tsol == 0:
-#         pass
-
-#     Ts.append(Tsol)
-#     Y1.append(
-#         x1
-#         * NRTL(x1, x2, Tsol, 7.79716106, -0.107372719, -3131.20054, 1540.90442, 0.3)
-#         * vapor_pressure_model(component_1["CASRN"], Tsol)
-#         / Patm
-#     )
-
-# plt.plot(X1, Ts, Y1, Ts)
-# plt.show()
-
-a, b = NRTL(0.5, 0.5, 300, 0, 0, 0, 0, 0)
-
-print(a)
