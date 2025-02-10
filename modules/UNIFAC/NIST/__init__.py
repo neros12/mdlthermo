74f02cda44f4eaf5f3ebfc5c92aca8844a3d268f
NewSeo <neros12@naver.com>
2025-02-10 11:29:18 +0900

    UNIFAC쪽 정리 끝끝

--- modules/UNIFAC/NIST/__init__.py
+++ modules/UNIFAC/NIST/__init__.py
@@ -1,1 +1,22 @@
-from .modules import cal_activity_coefficient as cal_NIST_UNIFAC
+from typing import Tuple
+
+from .modules import auto_fragment, UNIFAC
+
+
+def cal_binary_UNIFAC(
+    SMILES1: str,
+    SMILES2: str,
+    x1: float,
+    x2: float,
+    T: float,
+    check_temperature=False,
+) -> Tuple[float, float]:
+    component1 = auto_fragment(SMILES1)
+    component2 = auto_fragment(SMILES2)
+
+    return UNIFAC(
+        [component1, component2],
+        [x1, x2],
+        T,
+        check_temperature=check_temperature,
+    )
